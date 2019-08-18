31957088fdfc227aa2e6f5630c455d0f49718363
sandrig <sandrig.dev@gmail.com>
2019-08-18 12:12:57 +0300

    feat: add bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dynamic title';
+  img = 'https://miro.medium.com/max/512/1*gGh9I9ju9w4lXhmWoG2fXA.png';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over');
+      this.img = 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/thumb/angular2.png';
+    }, 3000);
+  }
 }
