06e1ad0fdb8661c3e781a0c93cbc39ef11b207f3
sandrig <sandrig.dev@gmail.com>
2020-09-07 20:35:02 +0300

    feat: add ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  posts = [
+    {
+      title: 'Post1',
+      author: 'Andrey',
+      comments: [
+        { name: 'Max', text: 'lorem 1' },
+        { name: 'Max', text: 'lorem 2' },
+        { name: 'Max', text: 'lorem 3' },
+      ]
+    },
+    {
+      title: 'Post2',
+      author: 'Andrey 2',
+      comments: [
+        { name: 'Max 2', text: 'lorem 1' },
+        { name: 'Max 2', text: 'lorem 2' },
+        { name: 'Max 2', text: 'lorem 3' },
+      ]
+    }
+  ];
 }
