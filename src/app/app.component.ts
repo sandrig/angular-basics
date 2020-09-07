0f1674d2cfa8c95d9f17b93394e1398141ddfa9c
sandrig <sandrig.dev@gmail.com>
2020-09-07 19:43:21 +0300

    feat: add two-way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Dynamic title';
-  // img = 'https://miro.medium.com/max/512/1*gGh9I9ju9w4lXhmWoG2fXA.png';
-  inputValue = '';
+  title = '';
 
-  constructor() {
-    // setTimeout(() => {
-    //   console.log('Timeout is over');
-    //   this.img = 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/thumb/angular2.png';
-    // }, 3000);
-  }
-
-  onInput(event: KeyboardEvent) {
-    console.log('Event', event);
-    this.inputValue = (event.target as HTMLInputElement).value;
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
-  }
-
-  onClick() {
-    console.log('Click!');
+  onInput(event: any) {
+    this.title = event.target.value;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { PostComponent } from './post/post.component';
     PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
