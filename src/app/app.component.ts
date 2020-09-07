ea81f332dabccdf83216add68d9dcc935284ed41
sandrig <sandrig.dev@gmail.com>
2020-09-07 21:50:23 +0300

    feat: add parameter passing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  posts: Post[] = [
+    {
+      title: 'I want to learn Angular components',
+      text: 'I am still learning components'
+    },
+    {
+      title: 'I want to learn React components',
+      text: 'I studied react components',
+      id: 2
+    },
+  ];
 }
