e64eb3feaf3b320c1fe8b89ae3f913e4e28a3867
sandrig <sandrig.dev@gmail.com>
2020-09-07 19:35:48 +0300

    feat: add event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dynamic title';
-  img = 'https://miro.medium.com/max/512/1*gGh9I9ju9w4lXhmWoG2fXA.png';
+  // img = 'https://miro.medium.com/max/512/1*gGh9I9ju9w4lXhmWoG2fXA.png';
+  inputValue = '';
 
   constructor() {
-    setTimeout(() => {
-      console.log('Timeout is over');
-      this.img = 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/thumb/angular2.png';
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log('Timeout is over');
+    //   this.img = 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/thumb/angular2.png';
+    // }, 3000);
+  }
+
+  onInput(event: KeyboardEvent) {
+    console.log('Event', event);
+    this.inputValue = (event.target as HTMLInputElement).value;
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str;
+  }
+
+  onClick() {
+    console.log('Click!');
   }
 }
