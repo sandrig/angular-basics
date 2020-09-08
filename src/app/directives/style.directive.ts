cd2c63fa835502164cc92f4fce988fc72f8a3e8e
sandrig <sandrig.dev@gmail.com>
2020-09-08 13:35:18 +0300

    feat: add getting access to element

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
-
+  constructor(private el: ElementRef, private r: Renderer2) {
+    this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+  }
 }
