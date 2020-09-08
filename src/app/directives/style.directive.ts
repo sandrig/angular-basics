db4ac29ae4c69bd5ed4e7c72b4d2d49e24365093
sandrig <sandrig.dev@gmail.com>
2020-09-08 14:23:08 +0300

    feat: add events

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,10 +1,21 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
   constructor(private el: ElementRef, private r: Renderer2) {
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log(event);
+  }
+
+  @HostListener('mouseenter') onEnter() {
     this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.r.setStyle(this.el.nativeElement, 'color', null);
   }
 }
