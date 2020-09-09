546a635ee707aacf49e6f1f5997529fe56c10c4a
sandrig <sandrig.dev@gmail.com>
2020-09-09 16:22:21 +0300

    feat: add custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4
-      }
-    }
-  };
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { MultByPipe } from './pipes/mult-by.pipe';
+import { ExMarksPipe } from './pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultByPipe,
+    MultByPipe,
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
