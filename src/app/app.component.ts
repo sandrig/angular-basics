4be41273beee52094ae94bd38d2395731d9936ba
sandrig <sandrig.dev@gmail.com>
2020-09-09 10:22:54 +0300

    feat: add object pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  float = 0.42;
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  };
 }
