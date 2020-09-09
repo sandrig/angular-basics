cda6c262fec409e71e5c4316ae095548121e1d8b
sandrig <sandrig.dev@gmail.com>
2020-09-09 10:05:23 +0300

    feat: add number pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostFormComponent,
-    PostComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
